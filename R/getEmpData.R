38c61d83a1257236d9f632331ac3f08c4a194d8c
manimurugan <manimurugan@constient.com>
2018-06-28 12:40:54 +0530

    asgsagsasagsag

--- R/getEmpData.R
+++ R/getEmpData.R
@@ -9,26 +9,19 @@
 #' @examples
 #'
 #' @export
+#'
+employee <- data.frame(
+  name = c('anandraj', 'Dhamodaran', 'Bharani', 'Arun', 'ragu'),
+  age = c(23, 20, 16, 27, 28),
+  salary = c(623.3,515.2,611.0,729.0,843.25),
+  start_date = as.Date(c("2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11",
+                         "2015-03-27")),
+  stringsAsFactors = FALSE
+)
 getEmpData <- function(e_name) {
-  employee <- data.frame(
-    name = c('anandraj', 'Dhamodaran', 'Bharani', 'Arun', 'ragu'),
-    age = c(23, 20, 16, 27, 28),
-    salary = c(623.3,515.2,611.0,729.0,843.25),
-    start_date = as.Date(c("2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11",
-                           "2015-03-27")),
-    stringsAsFactors = FALSE
-  )
   print(subset(employee, name==e_name))
 }
 
 getAllEmpData <- function() {
-  employee <- data.frame(
-    name = c('anandraj', 'Dhamodaran', 'Bharani', 'Arun', 'ragu'),
-    age = c(23, 20, 16, 27, 28),
-    salary = c(623.3,515.2,611.0,729.0,843.25),
-    start_date = as.Date(c("2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11",
-                           "2015-03-27")),
-    stringsAsFactors = FALSE
-  )
   print(employee)
 }
